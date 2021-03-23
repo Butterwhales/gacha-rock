2fa9623d37d8d73c0cb4940da05c86b6f2bd6ced
Butterwhales <Butterwhalesz@gmail.com>
2021-03-23 15:04:32 -0400

    Added summon page

--- app/src/main/java/com/example/gacha_rock/summon.java
+++ app/src/main/java/com/example/gacha_rock/summon.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class summon extends AppCompatActivity {
+    weightedRandom<String> rarity = new weightedRandom<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summon);
+
+        rarity.addEntry("Rock", 0.1);
     }
 }

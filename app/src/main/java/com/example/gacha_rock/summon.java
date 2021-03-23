1a6f7005689eb48d63bfe5b28b5cc169bcddbc15
Butterwhales <Butterwhalesz@gmail.com>
2021-03-23 15:01:55 -0400

    Added summon page

---
+++ app/src/main/java/com/example/gacha_rock/summon.java
@@ -0,0 +1,14 @@
+package com.example.gacha_rock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class summon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summon);
+    }
+}

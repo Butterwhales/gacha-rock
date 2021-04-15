4eeef30b8fe82951fbfa0c1d9e11140391c82b2a
Gabethunder3 <72380729+Gabethunder3@users.noreply.github.com>
2021-04-15 13:39:33 -0400

    rock_chan_icon.png

--- app/src/main/java/com/example/gacha_rock/store.java
+++ app/src/main/java/com/example/gacha_rock/store.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
